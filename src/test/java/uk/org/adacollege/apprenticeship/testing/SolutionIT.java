01a0a6e92cec9adf7d976d7330ad8c8e623f0eaf
d-bingley <d_bingley@hotmail.co.uk>
2017-06-14 10:34:01 +0100

    test for url

--- src/test/java/uk/org/adacollege/apprenticeship/testing/SolutionIT.java
+++ src/test/java/uk/org/adacollege/apprenticeship/testing/SolutionIT.java
@@ -100,9 +100,12 @@ public class SolutionIT {
     }
 
     private static void assertUrlEquals(String expectedUrl) {
-        // TODO: implement this method
-        // - use assertTitleEquals() as an example pattern to follow
-        // - search the web for how to find the current URL with Selenium
+        Boolean result = wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.getCurrentUrl().equals(expectedUrl);
+            }
+        });
+        assertTrue(result);
     }
 
     private static void assertElementTextEquals(By selector, String expectedText) {
